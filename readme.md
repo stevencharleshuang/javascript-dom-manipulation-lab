b000daa7380b60d195a3b10abca7ff3f17c44e2e
Steven Huang <steven.charles.huang@gmail.com>
2019-09-23 00:00:39 -0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,35 @@
+| Title | Type | Duration | Author |
+| -- | -- | -- | -- |
+| DOM Manipulation | Lab | 2:00 | Rachel Moskowitz (adapted from SEI NYC) |
+
+
+# ![](https://ga-dash.s3.amazonaws.com/production/assets/logo-9f88ae6c9c3871690e33280fcf557f33.png) DOM Manipulation Lab
+
+## Learning Objectives
+
+At the end of this lesson, students will be able to:
+- Manipulate the DOM.
+
+## Introduction
+
+Can you really **manipulate** the DOM, though?
+
+![Dom Toretto](https://media.giphy.com/media/XDb8RW95ZVLLW/giphy.gif)
+
+Didn't think so. Thankfully, we're not talking about **that** Dom.
+
+Today, we'll be practicing DOM manipulation and event handling.
+
+## Set Up
+
+You've been provided with a basic website, including the `index.html`, `style.css`, and `script.js` files. All of your work will go in the `script.js` file — you shouldn't have to edit `index.html` or `style.css`.
+
+There are empty functions for each part. Don't forget to **call** the function to test its utility.
+
+## Completion
+
+Finish filling out all of the functions set up in `script.js`.
+
+## Resources
+
+This [vanilla JavaScript quick reference and cheat sheet](https://gist.github.com/thegitfather/9c9f1a927cd57df14a59c268f118ce86) has a helpful section on DOM querying and creation.
---
+++ script.js
@@ -0,0 +1,87 @@
+let quotes = [
+  `I live my life a quarter mile at a time`,
+  `I said a ten-second car, not a ten-minute car`,
+  `You can have any brew you want... as long as it's a Corona.`,
+  `You almost had me? You never had me - you never had your car!`,
+  `I don't have friends. I have family.`,
+  `It don't matter if you win by an inch or a mile. Winning's winning.`,
+];
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  // Random quote of the day generator
+  function randomQuote() {
+    document.querySelector('#quote-of-the-day').textContent = `"${
+      quotes[Math.floor(Math.random() * quotes.length)]
+    }"`;
+  };
+  randomQuote();
+
+
+
+  // Do all of your work below in the following functions
+  // DON'T FORGET TO CALL THE FUNCTIONS TO SEE YOUR WORK!
+
+
+// Accessing DOM Elements (Querying)
+
+  // Part 1
+  // DOM's personal website title is a bit wordy. Write a JavaScript statement that selects the #main-title ID element. Change the text of the title to something shorter.
+  function shortenPageTitle() {
+
+  }
+
+
+  // Part 2
+  // Select the body and change the background-color to a new color of your choice.
+  function changeBodyBgColor() {
+
+  }
+
+
+  // Part 3
+  // Select DOM's Favorite Things list and remove the last list item.
+  function removeLastFavoriteThing() {
+
+  }
+
+
+  // Part 4
+  // Select all .special-title class elements and change their font-size to 2rem.
+  // Remember you might have to iterate through the list of elements
+  function makeSpecialTitlesBigger() {
+
+  }
+
+
+  // Part 5
+  // Turns out DOM never raced in Chicago. Access the Past Races list and remove Chicago.
+  function RemoveChicagoRace() {
+
+  }
+
+
+
+// Creating New DOM Elements
+
+  // Part 6
+  // Let's add to DOM's  Past Races  list.
+  // Create a new <li> element, change the new <li> text to the name of a city, and append it to the  Past Races  list.
+  function addPastRace() {
+
+  }
+
+
+  // Part 7
+  // Create a new .blog-post corresponding to the new city added in Part 6.
+  // You will have to create a new <div> with class of .blog-post, a new <h2> with text, and a new <p> with some text.
+  // Think about what order you want to create the elements, and what order you want to append them in.
+  function createNewBlogPost() {
+
+  }
+
+
+
+
+
+
+});
